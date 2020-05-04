b64100712f4cb103a8c9e17f342f06a048f7f62f
Matthew Lee <matthewlee626@gmail.com>
2020-05-04 08:14:15 -0700

    work for weekk 05 may

--- main.m
+++ main.m
@@ -14,7 +14,7 @@ figure, imagesc(sbdata.adj060);
 figure, imagesc(sbdata.adj090);
 figure, imagesc(sbdata.adj120);
 figure, imagesc(sbdata.adj150);
-%}
+
 
 test = sbdata.adj030;
 
@@ -22,3 +22,18 @@ test(test<0) = [0];
 
 G = graph(test);
 A = adjacency(G)
+%}
+
+idx030 = spectralcluster(sbdata.adj030, 2)
+idx060 = spectralcluster(sbdata.adj060, 2)
+idx090 = spectralcluster(sbdata.adj090, 2)
+idx120 = spectralcluster(sbdata.adj120, 2)
+idx150 = spectralcluster(sbdata.adj150, 2)
+
+% look at trends of accuracies
+% -1 -> 0 to 1
+% more datasets, more accuracy plots
+% malicious user
+
+figure, imagesc(sbdata.adj150);
+colorbar
